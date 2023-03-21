0384280c3ebcba12f189357d8d69dbe9a97c3f4a
Melaine Kanana <melaine.kanana@student.moringaschool.com>
2023-03-21 12:48:28 +0300

    DONE WITH ASSIGNMENT

--- index.js
+++ index.js
@@ -1,2 +1,23 @@
 
 // Write your code here
+
+// Multiply
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2 ;
+
+// Random
+
+let random;
+random = Math.floor(Math.random()) + 1;
+
+// Remainder
+let num3 = 9
+let num4 =5
+
+let mod = num3 % num4;
+
+// Max
+
+let max;
+max = Math.max(1, 2, 3, 4, 20);
